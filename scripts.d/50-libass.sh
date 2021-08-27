1788a4dea96be9737d23fb49b6572f9102ef2e98
ShiBen <807629978@qq.com>
2021-08-27 17:03:58 +0800

    update

--- scripts.d/50-libass.sh
+++ scripts.d/50-libass.sh
@@ -2,7 +2,7 @@
 
 #ASS_REPO="https://github.com/libass/libass.git"
 #ASS_COMMIT="a8456e673c473a081febb4c2da5ba90457574c6d"
-ASS_REPO="https://github.com/TheOneric/libass.git"
+ASS_REPO="https://github.com/BenLocal/libass.git"
 ASS_BRANCH= "unibreak"
 
 
